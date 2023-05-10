0e4bf4d3939e40d0f9d55b59b50e66b2bf3ca024
ntvs <NickRibs@hotmail.com>
2023-05-10 12:23:11 -0400

    Added function to get all schemas directly

--- index.js
+++ index.js
@@ -9,11 +9,20 @@ const schemas = [
 //Register schemas function to facilitate registering all schemas provided by this package
 const registerSchemas = (mongooseInstance) => {
     schemas.forEach(schema => {
-
         mongooseInstance.model(schema.name, schema.schema);
-
     });
 };
 
+//Function to facilitate getting all schemas directly. Returns an array of schemas.
+const getAllSchemas = () => {
+    let result = [];
+
+    schemas.forEach(schema => {
+        result.push(schema.schema);
+    });
+
+    return result;
+};
+
 //Export models
-module.exports = { registerSchemas, schemas };
+module.exports = { registerSchemas, getAllSchemas, schemas };
